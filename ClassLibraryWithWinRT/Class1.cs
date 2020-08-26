961e07afba404184fd13e4e53020c4ff685aa144
Miguel Ramos <miguelrb@microsoft.com>
2020-08-25 23:46:50 -0700

    Add project files.

---
+++ ClassLibraryWithWinRT/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibraryWithWinRT
+{
+    public class Class1
+    {
+        public string Value
+        {
+            get {
+
+                var s = Windows.Storage.UserDataPaths.GetDefault().Desktop;
+                return s; 
+            }
+        }
+    }
+}
---
+++ ConsoleAppBreakingChange/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppBreakingChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new ClassLibraryWithWinRT.Class1();
+            Console.WriteLine(c.Value);
+        }
+    }
+}
